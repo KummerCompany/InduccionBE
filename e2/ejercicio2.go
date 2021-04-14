e5f2a9a3e96c6a7d59411f609a54bafb9e951df7
Melina Griffo <meligriffo@gmail.com>
2021-04-14 10:55:52 -0300

    ejercicio dos terminado

---
+++ e2/ejercicio2.go
@@ -0,0 +1,18 @@
+//Ejercicio 2 : mostrar tipo de variables posibles
+//Author: MelinaRG
+
+package main
+
+import "fmt"
+
+func main() {
+	var edad int = 26
+	var nombre string = "Melina"
+	var altura float32 = 1.68
+	var mujer bool = true
+	fmt.Println(edad)
+	fmt.Println(nombre)
+	fmt.Println(altura)
+	fmt.Println(mujer)
+
+}
