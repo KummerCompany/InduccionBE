efbc5ac6ff296e32fd879ceca062d3cc644efbe6
Melina Griffo <meligriffo@gmail.com>
2021-04-19 09:15:34 -0300

    apellido agregado

--- e4/e4.go
+++ e4/e4.go
@@ -1,11 +1,17 @@
+//Ejercicio 4
+//Author: Melina Griffo
+
 package main
 
 import "fmt"
 
 func main() {
 	var nombre string
+	var apellido string
 	fmt.Println("Ingrese su nombre: ")
 	fmt.Scanf("%s\n", &nombre)
-	fmt.Printf("Hola %s\n", nombre)
+	fmt.Println("Ingrese su apellido: ")
+	fmt.Scanf("%s\n", &apellido)
+	fmt.Println("Hola", nombre, apellido)
 
 }
