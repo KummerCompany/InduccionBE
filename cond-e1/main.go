2014f85eea603f16fa3ecfb557223052f10b91ca
Melina Griffo <meligriffo@gmail.com>
2021-04-20 10:30:57 -0300

    Cond ejercicio1 terminado

---
+++ cond-e1/main.go
@@ -0,0 +1,40 @@
+//Ejercicio 1 - Condicionales
+//Author: Melina Griffo
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int
+	var b int
+	var c int
+	var d int
+
+	fmt.Println("Ingrese cuatro numeros diferentes: ")
+	fmt.Scanf("%d/n", &a)
+	fmt.Scanf("%d/n", &b)
+	fmt.Scanf("%d/n", &c)
+	fmt.Scanf("%d/n", &d)
+
+	if (a > b) && (a > c) && (a > d) {
+		fmt.Println("El numero mas grande es: ", a)
+
+	}
+
+	if (b > a) && (b > c) && (b > d) {
+		fmt.Println("El numero mas grande es: ", b)
+
+	}
+
+	if (c > a) && (c > b) && (c > d) {
+		fmt.Println("El numero mas grande es: ", c)
+
+	}
+
+	if (d > a) && (d > b) && (d > c) {
+		fmt.Println("El numero mas grande es: ", d)
+
+	}
+}
