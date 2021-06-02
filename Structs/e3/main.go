f0f82dc57cf46313ffeefa26b0116da2b1c32846
Melina <meligriffo@gmail.com>
2021-06-02 11:56:13 -0300

    Strcuts ejercicio3 terminado

---
+++ Structs/e3/main.go
@@ -0,0 +1,38 @@
+//Ejercicio dos - Structs
+//Author: Melina Griffo
+
+package main
+
+import "fmt"
+
+type User struct {
+	nombre, apellido, calle, ciudad string
+}
+
+func main() {
+
+	var value string
+
+	datos := new(User)
+
+	fmt.Println("Ingrese su nombre:")
+	fmt.Scanln(&value)
+	datos.nombre = value
+
+	fmt.Println("Ingrese su apellido:")
+	fmt.Scanln(&value)
+	datos.apellido = value
+
+	fmt.Println("Ingrese su domicilio:")
+	fmt.Scanln(&value)
+	datos.calle = value
+
+	fmt.Println("Ingrese su ciudad:")
+	fmt.Scanln(&value)
+	datos.ciudad = value
+
+	datos.calle = ""
+
+	fmt.Println(datos)
+
+}
